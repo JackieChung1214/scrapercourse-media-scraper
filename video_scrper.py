8f525901681575d357a0bc235a84efe2c5ae9565
Jackie Chung <jackiechung1214@gmail.com>
2022-07-20 19:36:13 +0800

    7.3 合併ts檔案順序有誤

--- video_scrper.py
+++ video_scrper.py
@@ -4,7 +4,7 @@ import glob
 
 
 #1.下載m3u8檔案
-response=requests.get('https://edgecast-cf-prod.yahoo.net/cp-video-transcode/production/b6b8d175-68aa-31d8-bb0a-9ced8fdd70b6/2022-06-30/07-08-18/e3f21603-23b4-5a4a-bc9b-5d8b6e8362ca/stream_640x360x856_v2.m3u8')
+response=requests.get('https://edgecast-cf-prod.yahoo.net/cp-video-transcode/production/61877050-d583-312d-bf21-e17b77107a57/2022-07-18/03-00-13/94027a58-9cf3-537d-9286-c54c64ed0c34/stream_640x360x440_v2.m3u8')
 
 if not os.path.exists('vedio'):
     os.mkdir('video')
@@ -16,7 +16,7 @@ with open ('video\\trailer.m3u8','wb') as file:
 ts_url_list=[]
 with open('video\\trailer.m3u8','r', encoding='utf-8') as file:
     contents=file.readlines()
-    base_url='https://edgecast-cf-prod.yahoo.net/cp-video-transcode/production/b6b8d175-68aa-31d8-bb0a-9ced8fdd70b6/2022-06-30/07-08-18/e3f21603-23b4-5a4a-bc9b-5d8b6e8362ca/'
+    base_url='https://edgecast-cf-prod.yahoo.net/cp-video-transcode/production/61877050-d583-312d-bf21-e17b77107a57/2022-07-18/03-00-13/94027a58-9cf3-537d-9286-c54c64ed0c34/'
 
     for content in contents:
         if content.endswith('ts\n'):
